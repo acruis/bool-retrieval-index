dd4a845c252fc6c961406f27955bf9c718353358
acruis <datsquall@gmail.com>
2016-03-02 20:18:43 +0800

    Input command line arguments in index.py

--- index.py
+++ index.py
@@ -1,1 +1,32 @@
 # Do indexing!
+
+import getopt
+import sys
+
+# def loadAllDocNames():
+
+def usage():
+	print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
+
+docs_dir = dict_file = postings_file = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+for o, a in opts:
+    if o == '-i':
+        docs_dir = a
+    elif o == '-d':
+        dict_file = a
+    elif o == '-p':
+        postings_file = a
+    else:
+        assert False, "unhandled option"
+if docs_dir == None or dict_file == None or postings_file == None:
+    usage()
+    sys.exit(2)
+
+print docs_dir
+print dict_file
+print postings_file
--- search.py
+++ search.py
@@ -1,1 +1,26 @@
-# Do search.py!
+
+def usage():
+	print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
+
+docs_dir = dict_file = postings_file = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+for o, a in opts:
+    if o == '-i':
+        docs_dir = a
+    elif o == '-d':
+        dict_file = a
+    elif o == '-p':
+        postings_file = a
+    else:
+        assert False, "unhandled option"
+if docs_dir == None or dict_file == None or postings_file == None:
+    usage()
+    sys.exit(2)
+
+print docs_dir
+print dict_file
+print postings_file
