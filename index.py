47fbb6d91ecbe1723c1199f1200d9c2f9debd0b1
acruis <datsquall@gmail.com>
2016-03-02 21:39:09 +0800

    Update postings for single term

--- index.py
+++ index.py
@@ -2,8 +2,29 @@
 
 import getopt
 import sys
+import nltk
+from os import listdir
+from os.path import isfile, join
+try:
+	import cPickle as pickle
+except:
+	import pickle
 
-# def loadAllDocNames():
+def loadAllDocNames(docs_dir):
+	joined_contents = [(f, join(docs_dir, f)) for f in listdir(docs_dir)]
+	joined_files = [(f, joined_f) for f, joined_f in joined_contents if isfile(joined_f)]
+	return joined_files
+
+def updatePostings(doc_name, postings):
+	docID, doc_path = doc_name
+	doc = file(doc_path).read()
+	sentences = nltk.tokenize.sent_tokenize(doc)
+	words = set([word for sentence in sentences for word in nltk.tokenize.word_tokenize(sentence)])
+	for word in words:
+		if word in postings:
+			postings[word].append(docID)
+		else:
+			postings[word] = [docID]
 
 def usage():
 	print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
@@ -30,3 +51,8 @@ if docs_dir == None or dict_file == None or postings_file == None:
 print docs_dir
 print dict_file
 print postings_file
+docs = loadAllDocNames(docs_dir)
+print docs[998]
+postings = {}
+updatePostings(docs[998], postings)
+print postings
